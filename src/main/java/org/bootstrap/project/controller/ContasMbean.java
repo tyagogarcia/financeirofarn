1415bf0ec2e5b1fe243302a610d049e56565baae
Lucas Oliveira <luksrn@gmail.com>
2012-12-12 03:00:54 -0300

    dev...

--- src/main/java/org/bootstrap/project/controller/ContasMbean.java
+++ src/main/java/org/bootstrap/project/controller/ContasMbean.java
@@ -1,5 +1,8 @@
 package org.bootstrap.project.controller;
 
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
 import org.bootstrap.project.domain.Conta;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -10,4 +13,9 @@ import com.github.javarch.jsf.AbstractCrudManagedBean;
 @Scope("view")
 public class ContasMbean extends AbstractCrudManagedBean<Conta>{
 
+	public DataModel<Conta> getFindAllContaUsuarioAsDataModel() {
+		DataModel<Conta> dataModel = new ListDataModel<Conta>();
+		// TODO pegar do usuario logado, pedente, ds.
+		return dataModel; 
+	} 
 }
