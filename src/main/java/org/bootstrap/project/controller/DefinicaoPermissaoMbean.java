f5177d595eb7ed5192091dee655bb41e27c5fe2e
Lucas Oliveira <luksrn@gmail.com>
2012-12-12 01:31:17 -0300

    Reajuste da tela, criacao de tela de definicao de permissa, ajustes para security.

---
+++ src/main/java/org/bootstrap/project/controller/DefinicaoPermissaoMbean.java
@@ -0,0 +1,10 @@
+package org.bootstrap.project.controller;
+
+import org.bootstrap.project.domain.Permissao;
+
+import com.github.javarch.jsf.AbstractCrudManagedBean;
+
+public class DefinicaoPermissaoMbean extends AbstractCrudManagedBean<Permissao>{
+
+	
+}
--- src/main/java/org/bootstrap/project/controller/UsuarioMbean.java
+++ src/main/java/org/bootstrap/project/controller/UsuarioMbean.java
@@ -9,21 +9,8 @@ import com.github.javarch.jsf.AbstractCrudManagedBean;
 @Component
 @Scope("view")
 public class UsuarioMbean extends AbstractCrudManagedBean<Usuario>{
-
-	private int idPapel;
-	
+ 
 	public UsuarioMbean() {		
 		super();
 	}
-
-	public int getIdPapel() {
-		return idPapel;
-	}
-
-	public void setIdPapel(int idPapel) {
-		this.idPapel = idPapel;
-	}
-
-	
-	
 }
