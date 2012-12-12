7f2f248c63ed12a9edf2b3d5274bd2f43f83813a
Lucas Oliveira <luksrn@gmail.com>
2012-12-12 12:12:49 -0300

    Adição de dependencia do Apache CXF, criação da autenticação via UserDetails. Correções nas páginas de listagem de contas a pagar/receber

--- src/main/java/org/bootstrap/project/domain/Usuario.java
+++ src/main/java/org/bootstrap/project/domain/Usuario.java
@@ -1,22 +1,27 @@
 package org.bootstrap.project.domain;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
-import javax.persistence.Transient;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.github.javarch.persistence.orm.hibernate.AbstractPersistable;
 
 @Entity
-public class Usuario extends AbstractPersistable  {	
+public class Usuario extends AbstractPersistable implements UserDetails {	
 	
 	private String username;
 	
 	private String senha;
 	
-	@OneToMany(fetch=FetchType.LAZY,mappedBy="usuario")
+	@OneToMany(fetch=FetchType.EAGER,mappedBy="usuario")
 	private List<Permissao> permissoes;
 	
 	@OneToMany(fetch=FetchType.LAZY,mappedBy="usuario")
@@ -53,7 +58,38 @@ public class Usuario extends AbstractPersistable  {
 	public void setContas(List<Conta> contas) {
 		this.contas = contas;
 	}
-	
-	
 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		List<GrantedAuthority> permissoes = new ArrayList<GrantedAuthority>();
+		for ( Permissao permissao : getPermissoes() ){
+			permissoes.add( new SimpleGrantedAuthority( permissao.getPapel().getDenominacao() ));
+		}
+		return permissoes;
+	}
+
+	@Override
+	public String getPassword() {
+		return senha;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
---
+++ src/main/java/org/bootstrap/project/service/UserDetailsService.java
@@ -0,0 +1,32 @@
+package org.bootstrap.project.service;
+
+import org.bootstrap.project.domain.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.github.javarch.persistence.orm.hibernate.HibernateRepository;
+
+@Service("userDetailsService")
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+
+	@Autowired
+	private HibernateRepository defaultRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username)
+			throws UsernameNotFoundException {
+		defaultRepository.setClazz(Usuario.class);
+		System.err.println("User details...");
+		Usuario usuario = new Usuario();
+		usuario.setUsername(username);
+		usuario = (Usuario) defaultRepository.findOneByExample(usuario);
+		System.err.println("Encontrou user " + usuario );
+		if ( usuario == null ){
+			throw new UsernameNotFoundException("");
+		}
+		return usuario;
+	}
+ 
+}
